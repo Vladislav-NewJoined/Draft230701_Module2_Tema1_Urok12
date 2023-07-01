8cd2e3835a54c21cd4e666e9f592ea3174bcdae7
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-01 20:06:11 +0300

    Initial commit

--- src/Draft0_Whole_Video_2_1_12.java
+++ src/Draft0_Whole_Video_2_1_12.java
@@ -1,5 +1,9 @@
-public class Main {
+public class Draft0_Whole_Video_2_1_12 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tВам нужно создать класс “Students” с полями “класс” и " +
+                "\n“оценка”, и попробовать вычислить средний балл у студентов при помощи различных " +
+                "\nметода (reduce, min/max и т.д.)\n\nРешение: ");
+
+
     }
 }
--- src/Draft1_Task2_1_12.java
+++ src/Draft1_Task2_1_12.java
@@ -1,4 +1,4 @@
-public class Draft0_Whole_Video_2_1_12 {
+public class Draft1_Task2_1_12 {
     public static void main(String[] args) {
         System.out.println("Задание : \n1.\tВам нужно создать класс “Students” с полями “класс” и " +
                 "\n“оценка”, и попробовать вычислить средний балл у студентов при помощи различных " +
@@ -7,3 +7,4 @@ public class Draft0_Whole_Video_2_1_12 {
         
     }
 }
+
