02e1b851338a7d63ad954ed557b1968f09e92d04
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-07-01 20:05:24 +0300

    Initial commit

---
+++ src/Draft1_Task2_1_12.java
@@ -0,0 +1,9 @@
+public class Draft0_Whole_Video_2_1_12 {
+    public static void main(String[] args) {
+        System.out.println("Задание : \n1.\tВам нужно создать класс “Students” с полями “класс” и " +
+                "\n“оценка”, и попробовать вычислить средний балл у студентов при помощи различных " +
+                "\nметода (reduce, min/max и т.д.)\n\nРешение: ");
+
+        
+    }
+}
